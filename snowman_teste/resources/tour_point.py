85a74fb860e86d42dfa36f01c86f55f5464406f2
felipeparaizo <felipeparaizo@gmail.com>
2017-09-28 01:40:53 -0300

    Checkpoint

--- snowman_teste/resources/tour_point.py
+++ snowman_teste/resources/tour_point.py
@@ -1,4 +1,8 @@
 import hug
+import json
+from falcon import HTTP_400
+# from snowman_teste.models import TourPoint, session
+# from snowman_teste.schemas import TourPointSchema
 
 
 class TourPointApi:
@@ -8,15 +12,33 @@ class TourPointApi:
 
     @hug.object.get()
     def get_all(self):
-        pass
+        try:
+            list_tour_point = session.query(TourPoint).all()
+            if len(list_tour_point) > 0:
+                tour_point_schema = TourPointSchema(many=True)
+                response = tour_point_schema.dump(list_tour_point).data
+                return response
+            else:
+                return {}
+        except Exception as error:
+            raise error
 
     @hug.object.get('{id}')
     def get_by_id(self, id: int):
         pass
 
     @hug.object.post()
-    def add(self):
-        pass
+    def add(self, body):
+        try:
+            if not body:
+                return {}
+            tour_point_schema = TourPointSchema(many=False)
+            tour_point = tour_point_schema.load(body, session=session).data
+            session.add(tour_point)
+            session.commit()
+            return tour_point_schema.dump(tour_point).data
+        except Exception as error:
+            raise error
 
     @hug.object.put()
     def update(self):
